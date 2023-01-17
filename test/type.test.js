f523884b0528cc8f028b94757af1b2b1fc95b543
Simon Petrac <pfoerdie@gmail.com>
2023-01-17 10:40:09 +0100

    initial commit

---
+++ test/type.test.js
@@ -0,0 +1,12 @@
+const
+    expect = require('expect'),
+    {describe, test} = require('mocha'),
+    type = require('../src/type.js');
+
+describe('fua.core.type', function () {
+
+    test.only('develop', function () {
+        console.log(type);
+    });
+
+});
