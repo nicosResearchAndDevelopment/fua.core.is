547861617899f51c9568e878a3f7b6ee7ce53fab
Simon Petrac <pfoerdie@gmail.com>
2023-01-17 17:07:40 +0100

    wip

--- src/type.js
+++ src/type.js
@@ -1,5 +1,22 @@
-exports.is = require('./type.is.js');
-exports.assert = require('./type.assert.js');
-exports.convert = require('./type.convert.js');
-exports.xsd = require('./type.xsd.js');
-Object.freeze(exports);
+const type = exports;
+
+type.is = require('./type.is.js');
+type.assert = require('./type.assert.js');
+type.convert = require('./type.convert.js');
+type.xsd = require('./type.xsd.js');
+
+module.exports = _freezeDeep(type);
+
+/**
+ * @template T
+ * @param {T} target
+ * @returns {Readonly<T>}
+ * @private
+ */
+function _freezeDeep(target) {
+    Object.freeze(target);
+    for (const key of Object.keys(target)) {
+        if (target[key] instanceof Object) _freezeDeep(target[key]);
+    }
+    return target;
+}
--- test/type.test.js
+++ test/type.test.js
@@ -6,7 +6,11 @@ const
 describe('fua.core.type', function () {
 
     test.only('develop', function () {
-        console.log(type);
+        // console.log(type);
+
+        // type.assert(false, 'test');
+        // type.assert.number.integer(10, 0, 100);
+        // type.assert.number.integer(10, {min: 0, max: 0});
     });
 
 });
